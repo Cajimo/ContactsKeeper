ee9fb0d5fccc1333d9d4329511f5acf593cbc987
Alex Mojica <alexmojicamtz@gmail.com>
2020-07-14 09:07:47 +0200

    Authenticate Route

--- routes/auth.js
+++ routes/auth.js
@@ -2,20 +2,73 @@
 // Gonna have 2 routes: 1) Gonna to be to just get the logged in user 2) Gonna to be actually log in the user and get the token.
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const { restart } = require('nodemon');
 
 // @route   GET  api/auth
 // @desc    Get logged in user
 // @access  Private
 router.get('/', (req, res) => {
-  res.send('Get logged in User');
+  res.send('Get logged in user');
 });
 
 // @route   POST  api/auth
 // @desc    Auth user & get token
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Log in User');
-});
+router.post(
+  '/',
+  [
+    check('email', 'Por favor, incluya un email válido').isEmail(),
+    check('password', 'Se requiere una clave').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: 'Datos inválidos' });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Datos inválidos' });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Error de Servidor');
+    }
+  }
+);
 
 // Have to export the router or won't work
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,75 @@
 //! Register route
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // @route   POST  api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Register a User');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Por favor, agregue un nombre').not().isEmpty(),
+    check('email', 'Por favor, incluya un email válido').isEmail(),
+    check(
+      'password',
+      'Por favor, ingrese una clave con 6 o más caracteres'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'El usuario ya existe' });
+      }
+      user = new User({
+        /* name: name,
+        email: email,
+        password: password, */
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(err.message);
+      res.status(500).send('Error de Servidor');
+    }
+  }
+);
 
 // Have to export the router or won't work
 module.exports = router;
