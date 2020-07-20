9452758c9e999d53c7bab4587cb14df09ec29825
Alex Mojica <alexmojicamtz@gmail.com>
2020-07-20 17:49:28 +0200

    Logout & Navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import AuthContext from '../../context/auth/authContext';
 
 const Navbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+
+  const { isAuthenticated, logout, user } = authContext;
+
+  const onLogout = () => logout();
+
+  const authLinks = (
+    <Fragment>
+      <li>Hola {user && user.name}</li>
+      <li>
+        <a onClick={onLogout} href="#!">
+          <i className="fas fa-sign-out-alt"></i>
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to="/registro">Registro</Link>
+      </li>
+      <li>
+        <Link to="/ingresar">Ingresar</Link>
+      </li>
+    </Fragment>
+  );
+
   return (
     <div className="navbar bg-primary">
       <h1>
         <i className={icon} /> {title}
       </h1>
       <ul>
-        <li>
+        {isAuthenticated ? authLinks : guestLinks}
+        {/*  <li>
           <Link to="/">Inicio</Link>
         </li>
         <li>
@@ -20,7 +51,7 @@ const Navbar = ({ title, icon }) => {
         </li>
         <li>
           <Link to="/ingresar">Ingresar</Link>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
